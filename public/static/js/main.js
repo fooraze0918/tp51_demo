9de067936867c95058ac24c3ce2e8ee43b7805bd
fuyz <392313545@qq.com>
2019-11-14 15:15:43 +0800

    static file --fuyz

---
+++ public/static/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * 公共css
+ */
+document.write('<link href="http://cdn.datatables.net/1.10.15/css/jquery.dataTables.css">');
+/**
+ * 公共js
+ */
+document.write('<script src="http://code.jquery.com/jquery-1.10.2.min.js"  type="text/javascript"></script>');
+document.write('<script src="http://cdn.datatables.net/1.10.15/js/jquery.dataTables.js"   type="text/javascript"></script>');
